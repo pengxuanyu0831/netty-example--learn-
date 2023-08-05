58065201b9fc45a9698e24a6d772737c732c0227
pengxuanyu <pengxuanyus@gmail.com>
2023-08-05 15:38:35 +0800

    Netty NIO 《Netty权威指南 p75》

---
+++ demo/src/main/java/org/demo/TimeClientHandler.java
@@ -0,0 +1,29 @@
+package org.demo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author xuanyu peng
+ * @description:
+ * @date 2023/8/5 15:23
+ */
+public class TimeClientHandler extends ChannelHandlerAdapter {
+
+
+    private final ByteBuf firstMessage;
+
+    public TimeClientHandler() {
+        byte[] bytes = "QUERY TIME ORDER".getBytes();
+        firstMessage = Unpooled.buffer(bytes.length);
+        firstMessage.writeBytes(bytes);
+    }
+
+
+    public void channelActive(ChannelHandlerContext msg) {
+        msg.writeAndFlush(firstMessage);
+    }
+
+}
