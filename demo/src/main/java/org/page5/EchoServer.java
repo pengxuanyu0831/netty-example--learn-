9c72df5711c456c75e38790b37d9ae03710e6757
pengxuanyu <pengxuanyus@gmail.com>
2023-08-12 20:22:08 +0800

    Netty NIO 《Netty权威指南》p95 EchoServer

---
+++ demo/src/main/java/org/page5/EchoServer.java
@@ -0,0 +1,55 @@
+package org.page5;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.example.echo.EchoServerHandler;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @author xuanyu peng
+ * @description:
+ * @date 2023/8/12 20:14
+ */
+public class EchoServer {
+
+
+    public void bind(int port) {
+        EventLoopGroup masterGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(masterGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new EchoServerHandler()).option(ChannelOption.SO_BACKLOG, 128).childHandler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+                    ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
+                    ch.pipeline().addLast(new StringDecoder());
+                    ch.pipeline().addLast(new EchoServerHandler());
+                }
+            });
+
+            ChannelFuture future = bootstrap.bind(port).sync();
+
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            masterGroup.shutdownGracefully();
+        }
+
+    }
+}
